15d938f396901d31526a1d9be41b48e904a2b286
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-23 22:08:48 +0200

    yas duzeltimi test netlify

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ function App() {
   return (
     <div>
       <h1>React Dersi I</h1>
-      <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness={5} />
-      <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness={3} />
+      <Person name="Ugur ARDA" jobs="Polis" age="25" sportiness={5} />
+      <Person name="Oktay ARDA" jobs="Ogrenci" age="22" sportiness={3} />
     </div>
   );
 }
