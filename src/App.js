280286079780b173ae649a78f7454c3c7dfd5578
Hussein Morsy <hussein@morsy.de>
2018-09-23 16:50:36 +0200

    Extract Stars component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function Person(props) {
       <h2>{props.name}</h2>
       <p>jobs {props.jobs}</p>
       <p>age {props.age}</p>
-      <p>sportiness {props.sportiness}</p>
+      <p>
+        sportiness <Stars number={props.sportiness} />
+      </p>
     </div>
   );
+}
+
+function Stars(props) {
+  return props.number;
 }
 
 function App() {
   return (
     <div>
       <h1>React Dersi I</h1>
-      <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness="5 " />
-      <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness="5 " />
+      <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness={5} />
+      <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness={3} />
     </div>
   );
 }
