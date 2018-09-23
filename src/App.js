cfd30cfb22e5b26be1704ea20e3cc9d62ebe5b81
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-23 17:37:24 +0200

    sportiness yildiz sekli

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React from "react";
 
 function Person(props) {
+  let yildiz;
+  for (let i = 0; i < props.sportiness; i++) {
+    yildiz = yildiz + "★️";
+  }
+
   return (
     <div>
       <h2>{props.name}</h2>
       <p>jobs {props.jobs}</p>
       <p>age {props.age}</p>
       <p>
-        sportiness <Stars numbers={props.sportiness} />
+        sportiness {yildiz}
+        />
       </p>
     </div>
   );
 }
 
-function Stars(props) {
-  let yildiz;
-  for (let i = 0; i < ; i++) {
-    yildiz = yildiz + "★️";
-  }
-  return yildiz;
-}
+// function Stars(props) {
+//   let yildiz;
+
+//   for (let i = 0; i < ; i++) {
+//     yildiz = yildiz + "★️";
+//   }
+//   return yildiz;
+// }
 
 function App() {
   return (
     <div>
       <h1>React Dersi I</h1>
       <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness={5} />
-      <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness={3} />
+      {/* <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness={3} />  */}
     </div>
   );
 }
