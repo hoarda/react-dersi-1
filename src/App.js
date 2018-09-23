be9cdeea554c6fc804cdbac6a1589f9863844c4b
Hussein Morsy <hussein@morsy.de>
2018-09-23 16:41:42 +0200

    Change html tags

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from "react";
 
 function Person(props) {
   return (
-    <li>
-      name {props.name}
-      jobs {props.jobs}
-      age {props.age}
-      sportiness {props.sportiness}
-    </li>
+    <div>
+      <h2>{props.name}</h2>
+      <p>jobs {props.jobs}</p>
+      <p>age {props.age}</p>
+      <p>sportiness {props.sportiness}</p>
+    </div>
   );
 }
+
 function App() {
   return (
     <div>
       <h1>React Dersi I</h1>
-      <ul>
-        <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness="5 " />
-        <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness="5 " />
-      </ul>
+      <Person name="Ugur ARDA " jobs="Polis " age="25 " sportiness="5 " />
+      <Person name="Oktay ARDA " jobs="Ogrenci " age="25 " sportiness="5 " />
     </div>
   );
 }
