b9cc6b42eff1dc575a06cac6eac47f69dd1d0b5b
Oktay Arda <hasanoktayarda@gmail.com>
2018-09-24 16:20:27 +0200

    fonksiyonu  if elseden kurtarmak

--- src/Heart.js
+++ src/Heart.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Heart(props) {
-  if (props.imo) return <span style={{ color: "red" }}> {"♥"}</span>;
-  else return <span style={{ color: "black" }}> {"♡"}</span>;
-  // return <span style={{ color: "red" }}> {"♥".if(props.imo === true)}</span>;
+  return props.imo === true ? (
+    <span style={{ color: "red" }}> {"♥"}</span>
+  ) : (
+    <span style={{ color: "black" }}> {"♡"}</span>
+  );
+  //   if (props.imo) return <span style={{ color: "red" }}> {"♥"}</span>;
+  //   else return <span style={{ color: "black" }}> {"♡"}</span>;
 }
 export default Heart;
