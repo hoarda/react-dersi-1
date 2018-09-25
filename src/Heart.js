6bd0ed343fc0a05dd17e4ed8b111812f8b71d313
Hussein Morsy <hussein@morsy.de>
2018-09-25 07:01:34 +0200

    Refactor Heart component

--- src/Heart.js
+++ src/Heart.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-function Heart(props) {
-  return props.imo === true ? (
-    <span style={{ color: "red" }}> {"♥"}</span>
-  ) : (
-    <span style={{ color: "black" }}> {"♡"}</span>
-  );
-  //   if (props.imo) return <span style={{ color: "red" }}> {"♥"}</span>;
-  //   else return <span style={{ color: "black" }}> {"♡"}</span>;
-}
-export default Heart;
+const redHeard = <span style={{ color: "red" }}> {"♥"}</span>;
+
+const blackHeard = <span style={{ color: "black" }}> {"♡"}</span>;
+
+export default props => (props.imo ? redHeard : blackHeard);
